efac0e64811fc720a42faa5aa4a8afe4e82a60f5
Kanth <kanthanawit.jaisong@gmail.com>
2024-10-08 21:44:33 +0700

    add throttle fucntion
    insert throttle to delay request

--- loaddata.R
+++ loaddata.R
@@ -1,30 +1,14 @@
+library(tidyverse)
+library(rvest)
+
+
+# get the area code and location
+# i found the NULL data with in this dataset
 lat.lon.subdi <- read_csv(file ="https://raw.githubusercontent.com/spicydog/thailand-province-district-subdistrict-zipcode-latitude-longitude/master/output.csv",
                           col_types = "cccnnn")
 
 all.marks <- lat.lon.subdi[c("latitude", "longitude")]
-
-
-library(rvest)
-loc <- read_html("https://data.tmd.go.th/nwpapi/doc/common/location_list1.html")
-
-css_selector_paragraph  <- "#book-search-results > div.search-noresults > section"
-
-loc.tble <- loc %>% 
-  html_element(css = css_selector_paragraph) %>% 
-  html_text() |>
-  readr::read_table(, skip = 1, col_names= c("code", "-" ,"loc_name"))
-
-
-
-loc %>% minimal_html() %>%
-  html_nodes(div id="book-search-results")
-
-
-
-#%>%
-  html_elements("li") %>% 
-  html_text2()
-#write the function to
+all.marks <- all.marks[complete.cases(all.marks),]
 
 # get the data ####
 
@@ -40,25 +24,31 @@ getWeather <- function(lat.n, lon.n){
                   date = "2024-10-09",
                   hour = 0,
                   duration = 24, .multi = "explode") |>
+    req_throttle(rate = 10/60) |>
     req_perform() |>
     resp_body_json()
   
-  error = function(e){
-    message(paste("An error occurred for item", i, stuff[[i]],":\n"), e)
-  }
+  
   
   weath <- json |> pluck(1, 1) |> pluck("forecasts") |> map_dfr(
     \(x){
       tibble(
         time = x |> pluck("time"),
-        cond = x |> pluck("data", "cond"),
-        rh = x |> pluck("data", "rh"),
         tc = x |> pluck("data", "tc"),
-        rain = x |> pluck("data", "rain")
+        rh = x |> pluck("data", "rh"),
+        rain = x |> pluck("data", "rain"),
+        cond = x |> pluck("data", "cond"),
       )
     }
   )
+  weath$lat <- json |> pluck(1, 1) |> pluck("location", "lat")
+  weath$lon <- json |> pluck(1, 1) |> pluck("location", "lon")
+  
+  weath %>% relocate(lat, lon)
 }
+
+lat.n <- 13.10
+lon.n <- 100.10
 
 mark.df <- tribble(~ lat.n, ~ lon.n, 
                    13.10, 100.10, 
@@ -67,15 +57,17 @@ mark.df <- tribble(~ lat.n, ~ lon.n,
 # retrieve
 list_DF <- list()
 #for (i in 60:nrow(all.marks)){
-for (i in 60:65){
+for (i in 1:10){
   print(i)
   list_DF[[i]] <- getWeather(lat.n = all.marks$latitude[i], 
                              lon.n = all.marks$longitude[i])
-  error <- function(e) return(NA)
 }
 
+saveRDS(list_DF, file = "TMD10.rds")
 rowbind <- list_DF |> list_rbind(names_to = "id") |> nest_by(id)
 
 
 # this code and reach to the limit
+
+
 
